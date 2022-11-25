eb8589caa7909881d88043a896ec829daa62fae8
bekbolotahahah <112771389+bekbolotahahah@users.noreply.github.com>
2022-11-26 00:04:14 +0600

    initial comit

---
+++ src/pages/catalogPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import classes from "./styles.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+const CatalogPage = () => {
+  return (
+    <div className={classes.conteyner}>
+    <nav className={classes.ToHome}>
+      <li ><Link to="/" >Home  </Link></li>
+      <li className={classes.iconn}><a href><FontAwesomeIcon className={classes.cart} icon={faChevronRight} /> </a></li>
+      <li className={classes.cat}>каталог</li>
+    </nav>
+    
+    <div className={classes.buner}>
+        <div className={classes.Muck}>
+                <h1>НОВАЯ СЕРИЯ McLAREN</h1>
+         </div>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default CatalogPage;
+
+     
---
+++ src/pages/homePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopPart from '../components/topPartHP';
+import Header  from '../components/header';
+
+const HomePage = () => {
+  return (
+    <div>
+      
+       <TopPart/> 
+    </div>
+  )
+}
+
+export default HomePage
